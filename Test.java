0b08cebcd8225235b92e4e7b3ffab3520cad3985
“selpiiee” <selvynurkhoffifa@gmail.com>
2025-10-02 14:30:23 +0700

    User 1

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] args) {
-        System.out.println("Hi, this is the first line of code");
-        System.out.println("User 2 add new line");
+        System.out.println("Hi, this is the first line of code modified by User 1");
+        System.out.println("User 1 added a new row");
     }
 }
