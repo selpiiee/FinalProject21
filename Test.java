313301c9ebe3b6570821faa48ef7e558b8d9de3b
luciakusumaw <itsmelucia27@gmail.com>
2025-10-02 14:22:46 +0700

    user 2

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hi, this is the first line of code");
+        System.out.println("User 2 add new line");
     }
 }
